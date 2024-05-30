a3347b98a77fad28b9da3c08ff926a5cbb33d535
Stanley Wang <ucheng.wang@gmail.com>
2024-05-30 03:39:21 +0000

    FIX - Apple Pay settings file

--- includes/gateways/class-payuni-payment-applepay.php
+++ includes/gateways/class-payuni-payment-applepay.php
@@ -53,7 +53,7 @@ class Payuni_Payment_ApplePay extends Payuni_Abstract_Payment_Gateway {
 	 * @return void
 	 */
 	public function init_form_fields() {
-		$this->form_fields = include WPBR_PAYUNI_PLUGIN_DIR . 'includes/settings/settings-payuni-payment-credit.php';
+		$this->form_fields = include WPBR_PAYUNI_PLUGIN_DIR . 'includes/settings/settings-payuni-payment-applepay.php';
 	}
 
 	/**
